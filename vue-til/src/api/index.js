c35cd7952763c9cdcb1e79590894a90c9f6cabe0
kimsejune <tpwns1088@gmail.com>
2021-07-21 21:02:02 +0900

    env 환경 설정

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+// vue cli에서 VUE_APP 시작하면 바로 가져올 수 있다.
+const instance = axios.create({
+	baseURL: process.env.VUE_APP_API_URL,
+});
+
 async function registerUser(userData) {
-	const url = 'http://localhost:3000/signup';
-	return await axios.post(url, userData);
+	return await instance.post('signup', userData);
 }
 
 export { registerUser };
