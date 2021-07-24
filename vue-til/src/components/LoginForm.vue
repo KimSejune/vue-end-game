fa899534a5a6c78370b1d28e8d7a590db1b11e52
kimsejune <tpwns1088@gmail.com>
2021-07-24 13:47:34 +0900

    css 적용

--- vue-til/src/components/LoginForm.vue
+++ vue-til/src/components/LoginForm.vue
@@ -1,16 +1,27 @@
 <template>
-	<form @submit.prevent="submitForm">
-		<div>
-			<label for="username">id:</label>
-			<input id="username" type="text" v-model="username" />
+	<div class="contents">
+		<div class="form-wrapper form-wrapper-sm">
+			<form @submit.prevent="submitForm" class="form">
+				<div>
+					<label for="username">id:</label>
+					<input id="username" type="text" v-model="username" />
+					<p class="validation-text">
+						<span class="warning" v-if="!isUsernameValid && username">
+							Please enter an email address
+						</span>
+					</p>
+				</div>
+				<div>
+					<label for="password">pw:</label>
+					<input id="password" type="text" v-model="password" />
+				</div>
+				<button class="btn" :disabled="!isFormValid" type="submit">
+					로그인
+				</button>
+			</form>
+			<p>{{ logMessage }}</p>
 		</div>
-		<div>
-			<label for="password">pw:</label>
-			<input id="password" type="text" v-model="password" />
-		</div>
-		<button :disabled="!isFormValid" type="submit">로그인</button>
-		<p>{{ logMessage }}</p>
-	</form>
+	</div>
 </template>
 
 <script>
@@ -57,4 +68,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.btn {
+	color: white;
+}
+</style>
--- vue-til/src/components/SignupForm.vue
+++ vue-til/src/components/SignupForm.vue
@@ -1,22 +1,26 @@
 <template>
-	<!-- prevent를 사용함으로써 제출후 새로고침을 막을 수 있다. -->
-	<form @submit.prevent="submitForm">
-		<div>
-			<label for="username">id: </label>
-			<input id="username" type="text" v-model="username" />
+	<div class="contents">
+		<div class="form-wrapper form-wrapper-sm">
+			<!-- prevent를 사용함으로써 제출후 새로고침을 막을 수 있다. -->
+			<form @submit.prevent="submitForm">
+				<div>
+					<label for="username">id: </label>
+					<input id="username" type="text" v-model="username" />
+				</div>
+				<div>
+					<label for="password">pw: </label>
+					<input id="password" type="text" v-model="password" />
+				</div>
+				<div>
+					<label for="nickname">nickname: </label>
+					<input id="nickname" type="text" v-model="nickname" />
+				</div>
+				<!-- button 클릭시 이벤트 버블링으로 form 태그에 올라간다. -->
+				<button :disabled="!isFormValid" type="submit">회원가입</button>
+			</form>
+			<p>{{ logMessage }}</p>
 		</div>
-		<div>
-			<label for="password">pw: </label>
-			<input id="password" type="text" v-model="password" />
-		</div>
-		<div>
-			<label for="nickname">nickname: </label>
-			<input id="nickname" type="text" v-model="nickname" />
-		</div>
-		<!-- button 클릭시 이벤트 버블링으로 form 태그에 올라간다. -->
-		<button :disabled="!isFormValid" type="submit">회원가입</button>
-		<p>{{ logMessage }}</p>
-	</form>
+	</div>
 </template>
 
 <script>
