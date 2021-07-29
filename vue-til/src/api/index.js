8eb2dadd5f00a3606bc0c3d5ba6d42c80799b8ed
kimsejune <tpwns1088@gmail.com>
2021-07-29 19:38:31 +0900

    token 저장 및 사용

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
+import { setInterceptors } from './common/interceptors';
 
-// vue cli에서 VUE_APP 시작하면 바로 가져올 수 있다.
-const instance = axios.create({
-	baseURL: process.env.VUE_APP_API_URL,
-});
+function createInstance() {
+	// vue cli에서 VUE_APP 시작하면 바로 가져올 수 있다.
+	const instance = axios.create({
+		baseURL: process.env.VUE_APP_API_URL,
+	});
+
+	return setInterceptors(instance);
+}
+
+const instance = createInstance();
 
 async function registerUser(userData) {
 	return await instance.post('signup', userData);
