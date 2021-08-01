2e4fc1927f5ca6a0e7a1d6c3f00f5c9152897ec0
kimsejune <tpwns1088@gmail.com>
2021-08-01 20:22:59 +0900

    get post list

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { setInterceptors } from './common/interceptors';
 
+// axios 초기화 함수
 function createInstance() {
 	// vue cli에서 VUE_APP 시작하면 바로 가져올 수 있다.
 	const instance = axios.create({
@@ -12,12 +13,19 @@ function createInstance() {
 
 const instance = createInstance();
 
+// 회원가입 API
 async function registerUser(userData) {
 	return await instance.post('signup', userData);
 }
 
+// 로그인 API
 async function loginUser(userData) {
 	return await instance.post('login', userData);
 }
 
-export { registerUser, loginUser };
+// 학습 노트 데이터를 조회하는 API
+function fetchPosts() {
+	return instance.get('posts');
+}
+
+export { registerUser, loginUser, fetchPosts };
