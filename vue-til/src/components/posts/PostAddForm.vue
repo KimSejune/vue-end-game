3394428e3cbae02f9ba4141f054f354cbbbd87dd
kimsejune <tpwns1088@gmail.com>
2021-08-04 23:30:56 +0900

    post API 구현

--- vue-til/src/components/posts/PostAddForm.vue
+++ vue-til/src/components/posts/PostAddForm.vue
@@ -2,23 +2,59 @@
 	<div class="contents">
 		<h1 class="page-header">Create Post</h1>
 		<div class="form-wrapper">
-			<form class="form">
+			<!-- prevent로 form 제출시 새로고침 방지 -->
+			<form class="form" @submit.prevent="submitForm">
 				<div>
 					<label for="title">Title:</label>
-					<input id="title" type="text" />
+					<input id="title" type="text" v-model="title" />
 				</div>
 				<div>
 					<label for="contents">Contents:</label>
-					<textarea id="contents" type="text" rows="5" />
+					<textarea id="contents" type="text" rows="5" v-model="contents" />
+					<p class="validataion-text warning" v-if="!isContentsValid">
+						Contents must be less than 200
+					</p>
 				</div>
 				<button type="submit" class="btn">Create</button>
 			</form>
+			<p class="log">
+				{{ logMessage }}
+			</p>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+import { createPost } from '@/api/index';
+
+export default {
+	data() {
+		return {
+			title: '',
+			contents: '',
+			logMessage: '',
+		};
+	},
+	computed: {
+		isContentsValid() {
+			return this.contents.length <= 200;
+		},
+	},
+	methods: {
+		async submitForm() {
+			try {
+				const postData = {
+					title: this.title,
+					contents: this.contents,
+				};
+				const response = await createPost(postData);
+				console.log(response);
+			} catch (error) {
+				this.logMessage = error.response.data.message;
+			}
+		},
+	},
+};
 </script>
 
 <style scoped>
